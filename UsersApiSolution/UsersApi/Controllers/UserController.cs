4077e494a1c9985f068e6f701635159e7cfe96ff
cumpelik_cngroup <mcumpelik@seznam.cz>
2020-07-08 16:42:47 +0200

    Fix

---
+++ UsersApiSolution/UsersApi/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UsersApi.Models;
+
+namespace UsersApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private UserDbAccessLayer UserDbAccess { get; } = new UserDbAccessLayer();
+
+        // GET: api/User
+        [HttpGet]
+        public async Task<IEnumerable<User>> Get()
+        {
+            return await UserDbAccess.GetUsersAsync();
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<User> Get(string id)
+        {
+            //return "value";
+            return await UserDbAccess.GetUserAsync(id);
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public async void Post([FromBody] User value)
+        {
+            if (UserDbAccess != null)
+            {
+                await UserDbAccess.InsertUserAsync(value);
+            }
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] User value)
+        {
+            UserDbAccess.UpdateUserAsync(id, value);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            UserDbAccess.DeleteUserAsync(id);
+        }
+    }
+}
---
+++ UsersApiSolution/UsersApi/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace UsersApi.Models
+{
+    public class User
+    {
+        public string Nickname { get; set; }
+        public string RealName { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+    }
+}
