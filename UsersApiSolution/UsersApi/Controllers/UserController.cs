7acf9b8eb5a3c4a46b4c2f7516ea0a48bbddb361
wally <ws.cesko@gmail.com>
2020-07-13 16:49:10 +0200

    Added missing project

--- UsersApiSolution/UsersApi/Controllers/UserController.cs
+++ UsersApiSolution/UsersApi/Controllers/UserController.cs
@@ -1,15 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-<<<<<<< HEAD
 using BusinessLogic.Core;
 using BusinessLogic.Logging;
 using Microsoft.AspNetCore.Mvc;
 using DbComm.Models;
 
-=======
-using Microsoft.AspNetCore.Mvc;
-using UsersApi.Models;
->>>>>>> 4077e494a1c9985f068e6f701635159e7cfe96ff
 
 namespace UsersApi.Controllers
 {
@@ -17,7 +12,6 @@ namespace UsersApi.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-<<<<<<< HEAD
         private readonly ILogger _logger;
 
         private readonly IDataProcessor _dataProcessor;
@@ -35,75 +29,38 @@ namespace UsersApi.Controllers
         {
             _logger.LogMessage("Controller GET called.");
             return await _dataProcessor.GetAllUsersAsync();
-=======
-        private UserDbAccessLayer UserDbAccess { get; } = new UserDbAccessLayer();
-
-        // GET: api/User
-        [HttpGet]
-        public async Task<IEnumerable<User>> Get()
-        {
-            return await UserDbAccess.GetUsersAsync();
->>>>>>> 4077e494a1c9985f068e6f701635159e7cfe96ff
         }
 
         // GET: api/User/5
         [HttpGet("{id}", Name = "Get")]
-<<<<<<< HEAD
         public async Task<UserDto> Get(string id)
         {
             _logger.LogMessage("Controller GET for one user called.");
             return await _dataProcessor.GetOneUserAsync(id);
-=======
-        public async Task<User> Get(string id)
-        {
-            //return "value";
-            return await UserDbAccess.GetUserAsync(id);
->>>>>>> 4077e494a1c9985f068e6f701635159e7cfe96ff
         }
 
         // POST: api/User
         [HttpPost]
-<<<<<<< HEAD
         public async Task Post([FromBody] string value)
         {
             _logger.LogMessage("Controller POST called.");
             await _dataProcessor.SaveNewPersonAsync(value);
-=======
-        public async void Post([FromBody] User value)
-        {
-            if (UserDbAccess != null)
-            {
-                await UserDbAccess.InsertUserAsync(value);
-            }
->>>>>>> 4077e494a1c9985f068e6f701635159e7cfe96ff
         }
 
         // PUT: api/User/5
         [HttpPut("{id}")]
-<<<<<<< HEAD
         public async Task Put(string id, [FromBody] string value)
         {
             _logger.LogMessage("Controller PUT called.");
             await _dataProcessor.UpdateExistingUserAsync(id, value);
-=======
-        public void Put(string id, [FromBody] User value)
-        {
-            UserDbAccess.UpdateUserAsync(id, value);
->>>>>>> 4077e494a1c9985f068e6f701635159e7cfe96ff
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-<<<<<<< HEAD
         public async Task Delete(string id)
         {
             _logger.LogMessage("Controller DELETE called.");
             await _dataProcessor.DeleteUserAsync(id);
-=======
-        public void Delete(string id)
-        {
-            UserDbAccess.DeleteUserAsync(id);
->>>>>>> 4077e494a1c9985f068e6f701635159e7cfe96ff
         }
     }
 }
--- UsersApiSolution/UsersApi/Startup.cs
+++ UsersApiSolution/UsersApi/Startup.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using BusinessLogic.Core;
 using BusinessLogic.Logging;
 using DbComm.Db;
@@ -11,23 +10,13 @@ using Microsoft.Extensions.DependencyInjection;
 using SimpleInjector;
 using SimpleInjector.Integration.AspNetCore.Mvc;
 
-=======
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
->>>>>>> 4077e494a1c9985f068e6f701635159e7cfe96ff
 
 namespace UsersApi
 {
     public class Startup
     {
-<<<<<<< HEAD
         public static Container Container { get; private set; } = new Container();
 
-=======
->>>>>>> 4077e494a1c9985f068e6f701635159e7cfe96ff
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -39,7 +28,6 @@ namespace UsersApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-<<<<<<< HEAD
 
 
             services.AddSingleton<IControllerActivator>(new SimpleInjectorControllerActivator(Container));
@@ -47,8 +35,6 @@ namespace UsersApi
                 options.AddAspNetCore()
                     .AddControllerActivation());
             services.UseSimpleInjectorAspNetRequestScoping(Container);
-=======
->>>>>>> 4077e494a1c9985f068e6f701635159e7cfe96ff
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,7 +51,6 @@ namespace UsersApi
 
             app.UseHttpsRedirection();
             app.UseMvc();
-<<<<<<< HEAD
 
             app.UseSimpleInjector(Container);
             Container.Register<ILogger, FileLogger>();
@@ -73,8 +58,6 @@ namespace UsersApi
             Container.Register<IUserDbAccessLayer, UserDbMongo>(); // TODO: what if I don't have a reference to the Db project? Can I register dependencies for it?
             
             Container.Verify();
-=======
->>>>>>> 4077e494a1c9985f068e6f701635159e7cfe96ff
         }
     }
 }
